89c4d5724216476c4fa7147f6a71d376a4f38e51
zhenwei1108 <46163072+zhenwei1108@users.noreply.github.com>
2019-11-18 22:20:06 +0800

    提交基础代码

---
+++ java-base/src/test/java/com/zhenwei/demo/javabase/JavaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhenwei.demo.javabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
